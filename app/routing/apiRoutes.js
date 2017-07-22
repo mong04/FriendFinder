9e81f35cc07e9c2d5b8bd8aece2cf8f2299fe92e
Andrew Sanchez <bballbizniz@aol.com>
2017-07-22 09:32:25 -0700

    newest testing code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,6 @@
 // Dependency
 const friends = require('../data/friends');
+const path = require('path');
 
 // API Routing
 module.exports = function(app) {
@@ -12,5 +13,33 @@ module.exports = function(app) {
     // POST survey data and find matches
     app.post('/api/friends', function(req, res) {
         
+        let bestMatch = {
+            name: '',
+            photo: '',
+            matchDiff: 500
+        }
+
+        let userData = req.body;
+
+        for (value of friends){
+            // console.log(value.name);
+            let matchVal = 0;
+
+            for (let j = 0; j < 10; j++){
+                console.log(value.scores[j]);
+                matchVal += Math.abs(parseInt(userData.scores[j]) 
+                - parseInt(value.scores[j]));
+
+                if (matchVal <= bestMatch.matchDiff) {
+                    bestMatch.name = value.name;
+                    bestMatch.photo = value.photo;
+                    bestMatch.matchDiff = matchVal;
+                }
+            }
+        }
+
+        friends.push(userData);
+
+        res.json(bestMatch);
     })
 }
