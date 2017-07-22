1a7948f5cba2370cc031cfac4e4250f601f1bd6c
Andrew Sanchez <bballbizniz@aol.com>
2017-07-22 13:49:24 -0700

    Adding more people to friends object

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 // Declare and create friends array
 
 var friends = [
-{
+    {
     "name":"Ahmed",
     "photo":"https://pbs.twimg.com/profile_images/779129231028682752/ijbnCAIa.jpg",
     "scores":[
@@ -16,6 +16,54 @@ var friends = [
         4,
         1
       ]
+    },
+    {
+        "name": "Joel Zimmerman",
+        "photo": "http://www.billboard.com/files/media/deadmau5-press-photo-2016-billboard-1548.jpg",
+        "scores": [
+        "1",
+        "5",
+        "4",
+        "3",
+        "2",
+        "5",
+        "2",
+        "5",
+        "2",
+        "1"
+        ]
+    },
+    {
+        "name": "Porter Robinson",
+        "photo": "http://www.billboard.com/files/media/porter-robinson-performance-2016-billboard-1548.jpg",
+        "scores": [
+        "3",
+        "3",
+        "5",
+        "1",
+        "5",
+        "2",
+        "4",
+        "2",
+        "4",
+        "1"
+        ]
+    },
+    {
+        "name": "Madeon",
+        "photo": "http://www.youredm.com/wp-content/uploads/2016/08/Madeon-9.jpg",
+        "scores": [
+            "3",
+            "3",
+            "5",
+            "2",
+            "5",
+            "3",
+            "5",
+            "2",
+            "4",
+            "1"
+            ]
     }
 ];
 
